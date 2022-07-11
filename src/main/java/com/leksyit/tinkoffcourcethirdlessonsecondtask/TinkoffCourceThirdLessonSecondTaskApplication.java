d91eff7d15ad5150bd054b0c7a1f44fabaa68f4d
Victor <80578735+LeksyIT@users.noreply.github.com>
2022-07-11 15:59:56 +0300

    Initial commit

---
+++ src/main/java/com/leksyit/tinkoffcourcethirdlessonsecondtask/TinkoffCourceThirdLessonSecondTaskApplication.java
@@ -0,0 +1,13 @@
+package com.leksyit.tinkoffcourcethirdlessonsecondtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinkoffCourceThirdLessonSecondTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TinkoffCourceThirdLessonSecondTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/leksyit/tinkoffcourcethirdlessonsecondtask/TinkoffCourceThirdLessonSecondTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leksyit.tinkoffcourcethirdlessonsecondtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkoffCourceThirdLessonSecondTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
